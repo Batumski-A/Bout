c644aa8c6c6fed984befbf3a4c389b2ae714fa14
Batumski-A <sharashidzelg@gmail.com>
2022-05-10 00:33:12 +0400

    Get in Controller Added Pages

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using Bout_2.Data;
+using Bout_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bout_2.Controllers
+{
+    [Route("Person")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        Person _person = new();
+        private readonly AppDbContext _context;
+        public PersonController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public Task<Person> AddBout(string FullNam,int Age,double Weight,bool Vaccined)
+        {
+            _person.FullName = FullNam;
+            _person.Age = Age;
+            _person.Weight = Weight;
+            _person.Vaccinated = Vaccined;
+            _context.Persons.Add(_person);
+            _context.SaveChanges();
+            return Task.FromResult(_person);
+        }
+
+    }
+}
